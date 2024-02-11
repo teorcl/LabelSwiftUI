6c1519d7cff7c8b61ec184b36d192115d54e6e04
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-02-11 14:31:28 -0500

    first Commit
    Add xcode project

---
+++ Label/Label/LabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelApp.swift
+//  Label
+//
+//  Created by Teodoro Calle Lara on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
